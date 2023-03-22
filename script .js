d406b56ca9bc5102c4c356e398e9f53a04a17d1e
aliridah9 <reda.haidar80@gmail.com>
2023-03-21 21:39:34 -0700

    first commit

---
+++ script .js
@@ -0,0 +1,8 @@
+function open_card(){
+    document.getElementById("inside").style.display="none";
+    document.getElementById("outside").style.display="block";
+}
+function close_card(){
+    document.getElementById("inside").style.display="block";
+    document.getElementById("outside").style.display="none";
+}
